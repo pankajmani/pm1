0b844abf299ec820d0bd3417b9c85e1e6a083e9a
pankajmani <pankajmani@users.noreply.github.com>
2016-02-23 23:56:10 -0500

    Update TestBankAccount.java

--- src/test/java/com/my/app/TestBankAccount.java
+++ src/test/java/com/my/app/TestBankAccount.java
@@ -9,14 +9,14 @@ public class TestBankAccount {
   @Test
   public void testDebitWithSufficientFunds(){
     BankAccount account = new BankAccount(10);
-    double Amount = account.debit(5);
+    double amount = account.debit(5);
     Assert.assertEquals(5.0, amount);
   }
   
   @Test
   public void testDebitWithoutSufficientFunds(){
     BankAccount account = new BankAccount(10);
-    double Amount = account.debit(11);
+    double amount = account.debit(11);
     Assert.assertEquals(10.0, amount);
   }
 }
